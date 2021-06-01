e2e12af29e4345233f4794f40c8d2c61f303d8f3
rafaelcarvalhocaetano <rapha.pse@outlook.com>
2021-05-31 21:40:57 -0300

    Updating files in app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
